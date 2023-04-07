aa9c5e2f2f0b1bc69380dc3f0c3a85b361bbaee4
Ederene20 <atinhoruben@gmail.com>
2023-04-07 18:12:00 +0100

    ref: remove useless comment

--- cmd/root.go
+++ cmd/root.go
@@ -12,8 +12,8 @@ var (
 	rootCmd = &cobra.Command{
 		Use:   "anx",
 		Short: "Find a solution to your bugs faster without opening your browser - Powered by ChatGPT",
-		Long: `A simple cli tool that will allow you to interact with ChatGPT from your terminal and solve
-				your bugs faster`,
+		Long: `Anx is a simple cli tool that allow you to interact with ChatGPT from your terminal and solve
+				your bugs faster.`,
 		// Run: func(cmd *cobra.Command, args []string) {
 		// 	fmt.Println("Anx - Powered by ChatGPT")
 		//},
