ef44a4e727e82dc377f592db11f4d173193e45c3
Ederene20 <atinhoruben@gmail.com>
2023-04-09 14:49:43 +0100

    ref: update set_api_key_script_location

--- cmd/set-api-key.go
+++ cmd/set-api-key.go
@@ -19,7 +19,7 @@ var setApiKeyCmd = &cobra.Command{
 	Short: "Set OpenAI API KEY",
 	Long:  `This command allow to set your OpenAI API KEY to be able to use Anx.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		set_api_key_script_location := os.Getenv("ANX_INSTALL") + "/bin/set-api-key.sh"
+		set_api_key_script_location := os.Getenv("ANX_INSTALL") + "/bin/scripts/set-api-key.sh"
 		output, err := exec.Command("/bin/bash", set_api_key_script_location, strings.Join(args, " ")).Output()
 
 		if err != nil {
